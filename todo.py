68bd7dc79e80fa1396647449cc08b34ab887277e
Ziga Ham <ziga@hamsworld.net>
2011-03-16 21:49:28 +0100

    Due date column is not always the 4th one

--- todo.py
+++ todo.py
@@ -12,7 +12,7 @@ PASSWORD = ''
 
 def parse_date(date):
 	months = {'januar':1, 'january':1, 'februar':2, 'february':2, 'marec':3, 'march':3, 'april':4, 'maj':5, 'may':5, 'junij':6, 'june':6, 'julij':7, 'july':7, 'avgust':8, 'august':8, 'september':9, 'oktober':10, 'october':10, 'november':11, 'december':12}
-	
+
 	date = [a.strip().split(' ') for a in date.lower().split(',')[1:]]
 	
 	day = date[0][0].replace('.', '')
@@ -66,36 +66,69 @@ tasks_all = 0
 for course in courses:
 	# check assigments
 	doc = BeautifulSoup(o.open(course[1].replace('course/view.php', 'mod/assignment/index.php'),  p).read().decode('utf8', 'replace'))
-	for assigment in doc.findAll('tr')[1:]: # first is header
-		namefield = assigment.find('td', 'c1')
-		if namefield: # if not its not actually an assigment
-			datefield = assigment.find('td', 'c3')
-			if datefield.text.strip() != '-': # if there is due date
-				date = parse_date(datefield.text)
-				if date >= datetime.now(): # whats gone is gone
-					submittedfield = assigment.find('td', 'c4')
-					if not submittedfield.find('span'): # if not already submitted
-						tasks.append((date, course[0], namefield.a.text))
-					else:
-						tasks_done += 1
-					tasks_all += 1
+	
+	table = doc.findAll('tr')
+	
+	if table:
+		#parse headers
+		c_namefield = -1
+		c_datefield = -1
+		c_submittedfield = -1
+		for c in range(10):
+			field = table[0].find('th', 'c%d' % c)
+			if field:
+				if field.text.strip() in ['Name', 'Ime']:
+					c_namefield = c
+				if field.text.strip() in ['Due date', 'Rok za oddajo']:
+					c_datefield = c
+				if field.text.strip() in ['Submitted', 'Oddano']:
+					c_submittedfield = c
+		
+		for assigment in table[1:]: # first is header
+			namefield = assigment.find('td', 'c%d' % c_namefield)
+			if namefield: # if not its not actually an assigment
+				datefield = assigment.find('td', 'c%d' % c_datefield)
+				if datefield.text.strip() not in ['-', '']: # if there is due date
+					date = parse_date(datefield.text)
+					if date >= datetime.now(): # whats gone is gone
+						submittedfield = assigment.find('td', 'c%d' % c_submittedfield)
+						if not submittedfield.find('span'): # if not already submitted
+							tasks.append((date, course[0], namefield.a.text))
+						else:
+							tasks_done += 1
+						tasks_all += 1
 	
 	# check quizes
 	doc = BeautifulSoup(o.open(course[1].replace('course/view.php', 'mod/quiz/index.php'),  p).read().decode('utf8', 'replace'))
-	for assigment in doc.findAll('tr')[1:]: # first is header
-		namefield = assigment.find('td', 'c1')
-		if namefield: # if not its not actually an assigment
-			datefield = assigment.find('td', 'c2')
-			if datefield.text.strip() not in ['-', '']: # if there is due date
-				date = parse_date(datefield.text)
-				if date >= datetime.now(): # whats gone is gone
-					# check if we already solved the quiz
-					doc = BeautifulSoup(o.open(BASE_URL + '/mod/quiz/' + dict(namefield.a.attrs)['href'],  p).read().decode('utf8', 'replace'))
-					if not doc.find('td', 'c0'):
-						tasks.append((date, course[0], namefield.a.text))
-					else:
-						tasks_done += 1
-					tasks_all += 1
+	
+	table = doc.findAll('tr')
+	
+	if table:
+		#parse headers
+		c_namefield = -1
+		c_datefield = -1
+		for c in range(10):
+			field = table[0].find('th', 'c%d' % c)
+			if field:
+				if field.text.strip() in ['Name', 'Ime']:
+					c_namefield = c
+				if field.text.strip() in ['Quiz closes', 'Kviz se zapre']:
+					c_datefield = c
+	
+		for assigment in table[1:]: # first is header
+			namefield = assigment.find('td', 'c%d' % c_namefield)
+			if namefield: # if not its not actually an assigment
+				datefield = assigment.find('td', 'c%d' % c_datefield)
+				if datefield.text.strip() not in ['-', '']: # if there is due date
+					date = parse_date(datefield.text)
+					if date >= datetime.now(): # whats gone is gone
+						# check if we already solved the quiz
+						doc = BeautifulSoup(o.open(BASE_URL + '/mod/quiz/' + dict(namefield.a.attrs)['href'],  p).read().decode('utf8', 'replace'))
+						if not doc.find('td', 'c0'):
+							tasks.append((date, course[0], namefield.a.text))
+						else:
+							tasks_done += 1
+						tasks_all += 1
 
 tasks.sort()
 	
